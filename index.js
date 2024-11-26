a7317d06f7800652ee80e91bb2d6037d6b471ad9
Isaac <your.email@example.com>
2024-11-27 00:06:12 +0300

    index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+// Function expression called divide
+const divide = function(a, b) {
+    return a / b;
+  };
+  
+  // Arrow function called square
+  const square = (x) => {
+    return x * x;
+  };
+  
+  // Arrow function called add
+  const add = (a, b) => {
+    return a + b;
+  };
+  
+  // Export the functions if you are using a module system (e.g., CommonJS)
+  // module.exports = { divide, square, add };
+  
